eb051bd9f904cd49f9b3b773714f598b3a95854e
medreres <90519417+medreres@users.noreply.github.com>
2023-02-06 00:55:32 +0200

    structuring folders

--- src/features/Recipes/api/saveRecipe.ts
+++ src/features/Recipes/api/saveRecipe.ts
@@ -1,9 +1,8 @@
-import { Recipe } from './../interface/index';
 import axios from "axios";
-
+import { Recipe } from "./../interface/index";
 export default async function searchRecipe(name: string, difficulty?: string) {
   // TODO difficulty
   return axios
-    .get("https://themealdb.com/api/json/v1/1/search.php?s=" + name)
+    .post("https://themealdb.com/api/json/v1/1/search.php?s=" + name)
     .then(({ data }) => (data.meals as Recipe[]) ?? []);
 }
